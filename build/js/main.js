0a881e38d0628e739249681c2e2603c7d471129d
mazax1ct <vova@giveandget.ru>
2024-04-03 19:29:21 +0300

    some blocks

--- build/js/main.js
+++ build/js/main.js
@@ -26,6 +26,7 @@ $(document).on('click', '.js-objects-menu-toggler', function () {
   return false;
 });
 
+//меню объектов
 var wrap = document.querySelector(".objects-menu__wrapper");
 var list = document.querySelector(".objects-menu__list");
 
@@ -68,7 +69,50 @@ function determineOverflow(content, container) {
 }
 
 const scrollContainer = document.querySelector(".js-horizontal-scroll");
+
 scrollContainer.addEventListener("wheel", (evt) => {
   evt.preventDefault();
   scrollContainer.scrollLeft += evt.deltaY;
+});
+
+let mouseDown = false;
+let startX, scrollLeft;
+const slider = document.querySelector('.js-horizontal-scroll');
+
+const startDragging = (e) => {
+  mouseDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+}
+
+const stopDragging = (e) => {
+  slider.classList.remove("is-dragging");
+  mouseDown = false;
+}
+
+const move = (e) => {
+  e.preventDefault();
+  if(!mouseDown) { return; }
+  const x = e.pageX - slider.offsetLeft;
+  const scroll = x - startX;
+  slider.scrollLeft = scrollLeft - scroll;
+  slider.classList.add("is-dragging");
+}
+
+slider.addEventListener('mousemove', move, false);
+slider.addEventListener('mousedown', startDragging, false);
+slider.addEventListener('mouseup', stopDragging, false);
+slider.addEventListener('mouseleave', stopDragging, false);
+
+//псевдо селект
+$(document).on('click', '.js-pseudo-select-toggler', function () {
+  $(this).closest('.pseudo-select').toggleClass('is-open');
+  return false
+});
+
+$(document).on('click', '.pseudo-select__item', function () {
+  $(this).closest('.pseudo-select').find('.pseudo-select__item').removeClass('is-active');
+  $(this).addClass('is-active');
+  $(this).closest('.pseudo-select').find('.pseudo-select__value span').text($(this).text());
+  return false
 });
